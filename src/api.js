e05cc347a3c9cdb283c569764737f32c07fd2572
KRAIG WILLIAMS <williamskrg@gmail.com>
2019-12-13 22:18:14 -0400

    almost done with apis.js

---
+++ src/api.js
@@ -0,0 +1,44 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/kraig';
+
+function getBookmarks () {
+    return fetch (`${BASE_URL}/bookmarks`); 
+}
+
+const createBookmark = function (title, url, descr, rating) {
+    const newBookmark = JSON.stringify({ title, url, descr, rating });
+    return fetch (`${BASE_URL}/bookmarks`, {
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json'
+        },
+        body: newBookmark
+    });
+}
+
+function updateBookmark (id, updateData) {
+    const update = JSON.stringify(updateData); 
+    return fetch (`${BASE_URL}/bookmarks/${id}`, {
+         method: 'PATCH',
+         headers: {
+             'Content-Type': 'application/json'
+         },
+         body: update
+     })
+ }
+
+ let deleteItem = function(id){
+    return fetch (`${BASE_URL}/items/${id}`, {
+      
+        method: 'DELETE',
+    
+   });
+        
+    };
+
+export default {
+    createBookmark,
+    getBookmarks,
+    updateBookmark,
+    deleteBookmark
+
+}
